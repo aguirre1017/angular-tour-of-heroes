9560b11931a2c101271cbe2cf5fdb6665097e01c
Carlo Arellano <carlo.arellano@hotmail.com>
2016-11-06 17:52:56 +0800

    Initial commit
    Codes mostly from Hero Editor Tutorial (Angular)

---
+++ app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Bane' },
+    { id: 12, name: 'Drow Ranger' },
+    { id: 13, name: 'Spirit Breaker' },
+    { id: 14, name: 'Rubrics' },
+    { id: 15, name: 'Lightning Revenant' },
+    { id: 16, name: 'Zeus' },
+    { id: 17, name: 'Magina' },
+    { id: 18, name: 'Phantom Assassin' },
+    { id: 19, name: 'Sand King' },
+    { id: 20, name: 'Ogre Magi' }
+];
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Bane' },
+  { id: 12, name: 'Drow Ranger' },
+  { id: 13, name: 'Spirit Breaker' },
+  { id: 14, name: 'Rubrics' },
+  { id: 15, name: 'Lightning Revenant' },
+  { id: 16, name: 'Zeus' },
+  { id: 17, name: 'Magina' },
+  { id: 18, name: 'Phantom Assassin' },
+  { id: 19, name: 'Sand King' },
+  { id: 20, name: 'Ogre Magi' }
+];
